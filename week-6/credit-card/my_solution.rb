735fc0f618bc23c3259c4739ace98125b26e14e3
Tal Schwartz <schwartztal@gmail.com>
2015-11-03 15:13:37 -0800

    reflect on 6.7

--- week-6/credit-card/my_solution.rb
+++ week-6/credit-card/my_solution.rb
@@ -2,7 +2,7 @@
 
 
 # I worked on this challenge [with: Brian Bensch].
-# I spent [#] hours on this challenge.
+# I spent [1.25] hours on this challenge.
 
 # Pseudocode
 
@@ -106,10 +106,11 @@ end
 
 end
 
-
-
-
-
-
-
 # Reflection
+=begin
+What was the most difficult part of this challenge for you and your pair?
+Agreeing on how we would structure the various steps of the algorithm across multiple methods. I was leaning towards a single method, Brian leaned towards splitting every action into its own method. In the end we started with the more verbose each action as a method and then refactored down to 3 methods.
+What new methods did you find to help you when you refactored?
+  using self. to call other methods of the class
+What concepts or learnings were you able to solidify in this challenge?
+.join and .split usage
