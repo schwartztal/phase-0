2d0b6e9ef0c5e27cdfc631965ffd55be2306a719
Tal Schwartz <schwartztal@gmail.com>
2015-11-10 20:31:32 -0800

    add eloquent.js in challenge 7.2 relase 3

---
+++ week-7/eloquent.js
@@ -0,0 +1,56 @@
+// Eloquent JavaScript
+
+// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+
+// Program Structure
+// Write your own variable and do something to it.
+
+// var testVar = 1;
+// console.log(testVar);
+// testVar = 2;
+// console.log(testVar);
+
+// var food = prompt("what's your favorite food");
+// alert("Hey! " + food + " is my favorite food too!");
+
+
+
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+
+
+// var number = 1
+// while (number <= 100){
+//   if (number % 3 == 0 && number % 5 == 0) console.log("FizzBuzz")
+//   else if (number % 3 == 0) console.log("Fizz");
+//   else if (number % 5 == 0) console.log("Buzz");
+//   else console.log(number);
+//   number += 1
+// }
+
+
+// Functions
+
+// Complete the `minimum` exercise.
+
+// function min(value1, value2){
+//   if (value1 > value2)
+//     return value1;
+//   else
+//     return value2;
+// }
+
+// console.log(min(0, 10));
+// // → 0
+// console.log(min(0, -10));
+// → -10
+
+// Data Structures: Objects and Arrays
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+var me = {
+name: "Tal Schwartz",
+age: 30,
+favoriteFoods: ["pizza", "ice cream", "whiskey"],
+quirk: "watches project runway"
+};
+
