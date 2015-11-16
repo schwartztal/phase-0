4f259ea21db42462e3ccf053f6dc2437c98cdf9b
Tal Schwartz <schwartztal@gmail.com>
2015-11-16 15:18:42 -0800

    complete 8.6

---
+++ week-8/ruby_review.rb
@@ -0,0 +1,15 @@
+=begin
+
+Solution to Drawer Debugger challenge in drawer_debugger.rb
+
+What concepts did you review or learn in this challenge?
+
+I completed the Drawer Debugger exercise: https://devbootcamp.instructure.com/courses/11/assignments/632?module_item_id=3970. It reviewed errors from Ruby and tracking down where those issues are in the code, then correcting them.
+
+What is still confusing to you about Ruby?
+
+Nothing at the moment, guess it's time to go learn some new concepts or get more practice.
+
+What are you going to study to get more prepared for Phase 1?
+
+Practice with syntax for Ruby and Javasript, my biggest weakness at the moment is correctly typing in code what I can think up in my head.
